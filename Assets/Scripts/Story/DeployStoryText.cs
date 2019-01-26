d0403fae7a5bda5e85bea7736034c1db850ec0fc
Pierre Galaup <contact@pierre-galaup.fr>
2019-01-26 22:25:30 +0100

    Ajust player size (area) + begin story manager

---
+++ Assets/Scripts/Story/DeployStoryText.cs
@@ -0,0 +1,42 @@
+﻿using Managers;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Story
+{
+    public class DeployStoryText : MonoBehaviour
+    {
+        [SerializeField]
+        private Canvas _storyCanvas;
+
+        private Text _text;
+
+        private readonly List<string> _storyline = new List<string>
+        {
+            "You survive since several months alone.\nHuman warmth begins to severely miss you...\nYou build a new home, in a safe place and you have to go out to find some equipment.\nAnd, why not, find some people to build a better home, a new family...",
+            "",
+            ""
+        };
+
+        private void Awake()
+        {
+            _text = GetComponent<Text>();
+            _storyCanvas.enabled = false;
+        }
+
+        private void Start()
+        {
+            if (GameManager.Instance.daysPassed == 0)
+            {
+                DisplayText(_storyline[0]);
+            }
+        }
+
+        public void DisplayText(string text)
+        {
+            _text.text = text;
+            _storyCanvas.enabled = true;
+        }
+    }
+}
