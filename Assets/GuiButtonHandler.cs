38cb277d86f1215e4ee5e9d3f0acaa466a73abec
Nicolas Garcia <nicolas.garcia.31@outlook.com>
2019-01-26 15:08:28 +0100

    Fix onClick for gui

--- Assets/GuiButtonHandler.cs
+++ Assets/GuiButtonHandler.cs
@@ -3,6 +3,7 @@ using Managers;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class GuiButtonHandler : MonoBehaviour
 {
@@ -12,26 +13,43 @@ public class GuiButtonHandler : MonoBehaviour
     [SerializeField]
     private MoveCarCamp moveCarCamp;
 
+    [SerializeField]
+    private Button exploreButton;
+
+    [SerializeField]
+    private Button shopButton;
+
     private GameManager _gameManager;
     
     private void Awake()
     {
         _gameManager = GameManager.Instance;
+        shopCanvas.SetActive(false);
     }
 
     public void ShowShop()
     {
+        if (shopCanvas.activeSelf)
+            return;
+        exploreButton.interactable = false;
+        shopButton.interactable = false;
         shopCanvas.SetActive(true);
     }
 
     public void StartExplore()
     {
+        shopButton.interactable = false;
+        exploreButton.interactable = false;
         moveCarCamp.MoveCar(LoadExplorationScene);
     }
 
     public void HideShop()
     {
+        if (!shopCanvas.activeSelf)
+            return;
         shopCanvas.SetActive(false);
+        exploreButton.interactable = true;
+        shopButton.interactable = transform;
     }
 
     private void LoadExplorationScene()
