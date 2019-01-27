bd4d426913791525d80faa65b70dfdebc8224b39
Pierre Galaup <contact@pierre-galaup.fr>
2019-01-27 10:37:27 +0100

    Add mummy, better player, better play area

---
+++ Assets/Scripts/Enemy/MummyMaterialSelector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class MummyMaterialSelector : MonoBehaviour
+    {
+        [SerializeField]
+        private List<Material> _materials;
+
+        private void Awake()
+        {
+            int index = Random.Range(0, _materials.Count);
+            GetComponent<SkinnedMeshRenderer>().material = _materials[index];
+        }
+    }
+}
